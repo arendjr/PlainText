58da9beb7824df00338883fb4b3f9c1c8f97f5f1
Arend van Beelen <arendjr@gmail.com>
2012-05-23 23:06:52 +0200

    Property/trigger editor.

--- web/main.js
+++ web/main.js
@@ -42,6 +42,64 @@ window.onblur = function() {
 };
 window.onfocus();
 
+String.prototype.trimmed = function() {
+
+    return this.replace(/^\s+|\s+$/g, "");
+};
+
+function prettyPrint(code) {
+
+    var indent = "";
+    var fromIndex = 0;
+    do {
+        var firstSemicolon = code.indexOf(";", fromIndex);
+        var firstOpeningBrace = code.indexOf("{", fromIndex);
+        var firstIndentedClosingBrace = code.indexOf("    }", fromIndex);
+        var firstClosingBrace = code.indexOf("}", fromIndex);
+
+        if (firstSemicolon === -1) {
+            firstSemicolon = 99999;
+        }
+        if (firstOpeningBrace === -1) {
+            firstOpeningBrace = 99999;
+        }
+        if (firstIndentedClosingBrace === -1) {
+            firstIndentedClosingBrace = 99999;
+        }
+        if (firstClosingBrace === -1) {
+            firstClosingBrace = 99999;
+        }
+
+        if (firstSemicolon < firstOpeningBrace && firstSemicolon < firstClosingBrace) {
+            code = code.substr(0, firstSemicolon + 1) + "\n" +
+                   indent + code.substr(firstSemicolon + 1).trimmed();
+            fromIndex = firstSemicolon + 1;
+        } else if (firstOpeningBrace < firstClosingBrace) {
+            indent += "    ";
+            code = code.substr(0, firstOpeningBrace + 1) + "\n" +
+                   indent + code.substr(firstOpeningBrace + 1).trimmed();
+            fromIndex = firstOpeningBrace + 1;
+        } else if (firstIndentedClosingBrace < firstClosingBrace) {
+            indent = indent.substr(0, indent.length - 4);
+            code = code.substr(0, firstIndentedClosingBrace) + "}\n" +
+                   indent + code.substr(firstIndentedClosingBrace + 5).trimmed();
+            fromIndex = firstIndentedClosingBrace + 2;
+        } else {
+            indent = indent.substr(0, indent.length - 4);
+            code = code.substr(0, firstClosingBrace + 1) + "\n" +
+                   indent + code.substr(firstClosingBrace + 1).trimmed();
+            fromIndex = firstClosingBrace + 1;
+        }
+    } while (firstSemicolon < 99999 ||
+             firstOpeningBrace < 99999 ||
+             firstClosingBrace < 99999);
+
+    code = code.replace(/}\s+(else|while)/g, "} $1");
+    code = code.replace(/}\s+(\)|,)/g, "}$1");
+
+    return code;
+}
+
 function Controller() {
 
     this.screen = document.getElementsByClassName("screen")[0];
@@ -55,13 +113,41 @@ function Controller() {
 
     this.history = [];
     this.historyIndex = 0;
-    this.currentCommand = '';
+    this.currentCommand = "";
+
+    this.editScreen = document.querySelector(".edit-screen");
+    this.editField = document.getElementById("edit-field");
+    this.setCommand = "";
+    this.onMessageHook = null;
 
     var self = this;
     this.commandInput = document.getElementsByClassName("command-input")[0];
     this.commandInput.onkeypress = function(event) {
         if (event.keyCode === keys.KEY_RETURN) {
             var command = self.commandInput.value;
+
+            if (self.player.isAdmin) {
+                if (command.substr(0, 10) === "edit-prop " ||
+                    command.substr(0, 13) === "edit-trigger ") {
+
+                    self.setCommand = "set" + command.substr(4);
+                    command = "get" + command.substr(4);
+
+                    self.editScreen.style.display = "block";
+
+                    self.onMessageHook = function(message) {
+                        message = message.trimmed();
+                        if (message.substr(0, 9) === "(function" &&
+                            message.substr(-2) === "})") {
+                            message = prettyPrint(message);
+                        }
+
+                        self.editField.value = message;
+                        self.editField.focus();
+                    }
+                }
+            }
+
             self.socket.send(command);
             self.commandInput.value = "";
 
@@ -117,11 +203,36 @@ function Controller() {
 
                 if (self.player.isAdmin) {
                     self.commandInput.removeAttribute("maxlength");
+
+                    document.getElementById("edit-cancel-button").onclick = function() {
+                        self.editScreen.style.display = "none";
+                        self.commandInput.focus();
+                    }
+                    document.getElementById("edit-submit-button").onclick = function() {
+                        self.socket.send(self.setCommand + " " + self.editField.value);
+
+                        self.editScreen.style.display = "none";
+                        self.commandInput.focus();
+                    }
+                    self.editField.onkeydown = function(event) {
+                        if (event.keyCode === keys.KEY_TAB) {
+                            var value = self.editField.value;
+                            var start = self.editField.selectionStart;
+                            var end = self.editField.selectionEnd;
+                            self.editField.value = value.substr(0, start) + "    " + value.substr(end);
+                            self.editField.selectionStart = start + 4;
+                            self.editField.selectionEnd = start + 4;
+                            return false;
+                        }
+                    }
                 }
 
                 self.statusHeader.name.innerText = self.player.name;
                 self.statusHeader.hp.innerText = self.player.hp + "HP";
             }
+        } else if (self.onMessageHook) {
+            self.onMessageHook(message.data);
+            self.onMessageHook = null;
         } else {
             self.writeToScreen(message.data);
         }
