72e3b17265c2526e84446e787200a8f805e3c866
Arend van Beelen <arendjr@gmail.com>
2012-07-08 13:45:50 +0200

    Handle sign in properly in events.

---
+++ src/engine/signinevent.cpp
@@ -0,0 +1,30 @@
+#include "signinevent.h"
+
+#include <QDebug>
+
+#include "session.h"
+
+
+SignInEvent::SignInEvent(Session *session, const QString &input) :
+    Event(),
+    m_session(session),
+    m_input(input) {
+}
+
+SignInEvent::~SignInEvent() {
+}
+
+void SignInEvent::process() {
+
+    if (!m_session || m_input.isEmpty()) {
+        qWarning() << "Processing uninitialized sign in event. Skipped.";
+        return;
+    }
+
+    m_session->processSignIn(m_input);
+}
+
+QString SignInEvent::toString() const {
+
+    return QString("Sign in input during stage %1: %2").arg(m_session->signInStage()).arg(m_input);
+}
