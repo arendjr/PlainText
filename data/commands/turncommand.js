9e527f950f0c1135ac2142c8c40112362679f686
Arend van Beelen <arendjr@gmail.com>
2013-01-30 15:40:42 +0100

    Add turn command.

---
+++ data/commands/turncommand.js
@@ -0,0 +1,32 @@
+function TurnCommand() {
+    Command.call(this);
+
+    this.setDescription("Turn to face some other direction.\n" +
+                        "\n" +
+                        "Examples: turn left, turn right, turn around");
+}
+
+TurnCommand.prototype = new Command();
+TurnCommand.prototype.constructor = TurnCommand;
+
+TurnCommand.prototype.execute = function(player, command) {
+
+    this.prepareExecute(player, command);
+
+    var direction = this.takeWord();
+
+    if (direction === "left") {
+        player.direction = [player.direction[1], -player.direction[0], player.direction[2]];
+    } else if (direction === "right") {
+        player.direction = [-player.direction[1], player.direction[0], player.direction[2]];
+    } else if (direction === "around") {
+        player.direction = [-player.direction[0], -player.direction[1], -player.direction[2]];
+    } else {
+        this.send("Turn where?");
+        return;
+    }
+
+    this.send(player.currentRoom.lookAtBy(player));
+};
+
+CommandRegistry.registerCommand("turn", new TurnCommand());
