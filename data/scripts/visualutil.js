28ba7000abf5791833e07660aec9b14afa5415fe
Arend van Beelen <arendjr@gmail.com>
2013-02-05 16:46:15 +0100

    Fine-tune displaying of groups.

--- data/scripts/visualutil.js
+++ data/scripts/visualutil.js
@@ -116,7 +116,8 @@ var VisualUtil = (function() {
                 if (characterInfo.group) {
                     name = characterInfo.group.nameAtStrength(characterInfo.strength);
                     if (numPeople > 0 || i < group.length - 1) {
-                        name = "a group of " + (name.startsWith("some ") ? name.mid(5) : name);
+                        name = "a group " + (name.startsWith("a lot ") ? "with " : "of ") +
+                                            (name.startsWith("some ") ? name.mid(5) : name);
                     }
 
                     actionDescription = describeActionRelativeTo(characterInfo.group.leader,
